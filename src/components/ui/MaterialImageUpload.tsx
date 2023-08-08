772621156c1f923e74bfe6b03330895964026be3
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2023-08-08 22:45:35 +0300

    auto commit

--- src/components/ui/MaterialImageUpload.tsx
+++ src/components/ui/MaterialImageUpload.tsx
@@ -6,13 +6,12 @@ import { ImagePlus, Trash } from "lucide-react";
 import { CldUploadWidget } from "next-cloudinary";
 import { IconButton } from "@mui/material";
 import { Skeleton } from "./skeleton";
-import Image from "next/image";
 
 interface MaterialImageUploadProps {
   disabled?: boolean;
   onChange: (value: string) => void;
-  onRemove: (value: string) => void;
-  value: string;
+  onRemove: () => void;
+  value?: string;
 }
 
 const MaterialImageUpload: React.FC<MaterialImageUploadProps> = ({
@@ -35,27 +34,19 @@ const MaterialImageUpload: React.FC<MaterialImageUploadProps> = ({
 
   return (
     <div>
-      <div className="mb-4 flex items-center gap-4 p-4">
-        <div
-          key={value}
-          className="relative h-[200px] w-[200px] overflow-hidden rounded-md"
-        >
-          <div className="absolute right-2 top-2 z-10">
-            <Button
-              type="button"
-              onClick={() => onRemove(value)}
-              variant="destructive"
-              size="icon"
-            >
-              <Trash className="h-4 w-4" />
-            </Button>
+      <div className="flex items-center justify-between gap-4 p-4">
+        {value && value.length > 0 ? (
+          <div className="flex gap-4 rounded-md">
+            <img alt="user image" src={value} />
+            <div className="">
+              <IconButton onClick={() => onRemove()} color="warning">
+                <Trash className="h-4 w-4" />
+              </IconButton>
+            </div>
           </div>
-          {value.length > 0 ? (
-            <Image fill className="object-cover" alt="Image" src={value} />
-          ) : (
-            <Skeleton className="h-[200px] w-[200px]"></Skeleton>
-          )}
-        </div>
+        ) : (
+          <Skeleton className="h-[144px] w-[256px] rounded-md" />
+        )}
         <CldUploadWidget onUpload={onUpload} uploadPreset="zaibke97">
           {({ open }) => {
             const onClick = () => {
@@ -70,7 +61,7 @@ const MaterialImageUpload: React.FC<MaterialImageUploadProps> = ({
                 onClick={onClick}
               >
                 <ImagePlus className="mr-2 h-4 w-4" />
-                Upload an image
+                Upload
               </Button>
             );
           }}
