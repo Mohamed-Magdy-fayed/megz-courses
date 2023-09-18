45ce9020aacdc59394ca2e87e0ca5d4bb4e95b65
megz <mohamed.ahmaed.sc2@gmail.com>
2023-09-18 15:13:46 +0300

    auto commit

--- src/pages/api/facebook.ts
+++ src/pages/api/facebook.ts
@@ -1,13 +1,31 @@
+import { prisma } from "@/server/db";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-    value: number;
+    value: string | string[] | undefined;
 };
 
-const POST = (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    console.log(req.body);
+const handler = (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    switch (req.method) {
+        case "GET":
+            console.log("GET request");
+            console.log("req.query.hub_challenge", req.query.hub_challenge);
+            console.log("req.query.hub_verify_token", req.query.hub_verify_token);
 
-    res.status(200).json({ value: 2013469864 });
+            res.status(200).json({ value: req.query.hub_challenge });
+            break;
+        case "POST":
+            console.log("POST request");
+            prisma.facebook.create({
+                data: { data: JSON.stringify(req.body) }
+            })
+
+            res.status(200).json({ value: req.body });
+            break;
+
+        default:
+            break;
+    }
 };
 
-export default POST
+export default handler;
