1028d40dae968f7127965df8058661083a98db94
megz <mohamed.ahmaed.sc2@gmail.com>
2023-10-04 18:51:43 +0300

    disable tabs

--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -1,7 +1,6 @@
 import { ConceptTitle } from "@/components/ui/Typoghraphy";
 import { ApiAlert } from "@/components/ui/api-alert";
 import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import AppLayout from "@/layouts/AppLayout";
 import { Typography } from "@mui/material";
 import { ArrowRightFromLineIcon } from "lucide-react";
@@ -12,22 +11,11 @@ const ConfigPage: NextPage = () => {
         <AppLayout>
             <ConceptTitle>Configure Facebook webhooks</ConceptTitle>
             <Typography></Typography>
-            <Tabs defaultValue="facebook" className="w-full mt-8 p-4">
-                <TabsList>
-                    <TabsTrigger value="facebook">Facebook</TabsTrigger>
-                    <TabsTrigger value="other">Other</TabsTrigger>
-                </TabsList>
-                <TabsContent value="facebook">
-                    <ApiAlert title="webhook callback url" description="https://megz-courses.vercel.app/api/facebook" />
-                    <div className="flex items-center justify-between p-4">
-                        <Typography>once verified successfully your potintial customers will be added to your database</Typography>
-                        <Button>Go to Database <ArrowRightFromLineIcon></ArrowRightFromLineIcon></Button>
-                    </div>
-                </TabsContent>
-                <TabsContent value="other">
-                    Comming Soon...
-                </TabsContent>
-            </Tabs>
+            <ApiAlert title="webhook callback url" description="https://megz-courses.vercel.app/api/facebook" />
+            <div className="flex items-center justify-between p-4">
+                <Typography>once verified successfully your potintial customers will be added to your database</Typography>
+                <Button>Go to Database <ArrowRightFromLineIcon></ArrowRightFromLineIcon></Button>
+            </div>
         </AppLayout>
     )
 }
