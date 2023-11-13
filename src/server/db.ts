864a5a04127b52b332713d86eabef4a282a006dd
megz <mohamed.ahmaed.sc2@gmail.com>
2023-11-13 09:37:15 +0200

    auto commit

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { MongoClient, Db } from 'mongodb';
 import { env } from "@/env.mjs";
 
 const globalForPrisma = globalThis as unknown as {
@@ -13,31 +12,3 @@ export const prisma =
   });
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-// const DATABASE_URL = env.DATABASE_URL
-
-// const globalForMongo = globalThis as unknown as {
-//   mongoClient: MongoClient | undefined;
-//   mongoDb: Db | undefined;
-// };
-
-// export const mongoClient =
-//   globalForMongo.mongoClient ??
-//   new MongoClient(DATABASE_URL);
-
-// export const mongoDb = globalForMongo.mongoDb ?? mongoClient.db();
-
-// // Connect to the MongoDB server
-// (async () => {
-//   try {
-//     if (!globalForMongo.mongoClient) {
-//       await mongoClient.connect();
-//       console.log('Connected to MongoDB');
-//       globalForMongo.mongoClient = mongoClient;
-//       globalForMongo.mongoDb = mongoDb;
-//     }
-//   } catch (error) {
-//     console.error('Error connecting to MongoDB:', error);
-//     throw error;
-//   }
-// })();
