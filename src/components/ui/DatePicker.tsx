e1b2b90d106ed450a484dc673d69cc40f1e82c25
megz <mohamed.ahmaed.sc2@gmail.com>
2024-05-08 16:48:51 +0300

    auto commit

--- src/components/ui/DatePicker.tsx
+++ src/components/ui/DatePicker.tsx
@@ -2,7 +2,7 @@ import { Dispatch, FC, SetStateAction } from "react"
 import { format } from "date-fns"
 
 import { Button } from "@/components/ui/button"
-import { Calendar } from "@/components/ui/calendar"
+import Calendar from "@/components/ui/calendar"
 import {
     Popover,
     PopoverContent,
