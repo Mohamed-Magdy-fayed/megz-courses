c05208edeada67243284356cd57de721d6589676
Mohamed-Magdy-fayed <mohamed.ahmed.sc2@gmail.com>
2024-09-18 22:02:19 +0300

    auto commit

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,7 +1,4 @@
 import LandingLayout from '@/components/landingPageComponents/LandingLayout';
-import { ConceptTitle, Typography } from '@/components/ui/Typoghraphy';
-import { env } from '@/env.mjs';
-import { format } from 'date-fns';
 import Head from 'next/head';
 
 const PrivacyPolicyPage = () => {
@@ -12,110 +9,12 @@ const PrivacyPolicyPage = () => {
                 <meta name="description" content="Privacy Policy for Megz Courses" />
                 <meta name="robots" content="index, follow" />
             </Head>
-            <div className="p-12 space-y-8">
-                <ConceptTitle className="text-2xl font-semibold mb-4">Privacy Policy</ConceptTitle>
-
-                <Typography><strong>Effective Date:</strong> {format(new Date("7/aug/2024"), "PPP")}</Typography>
-
-                <div>
-                    <Typography variant={"secondary"}>1. Introduction</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Welcome to Megz Courses! This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our app integrated with Meta (formerly Facebook) services, including our Facebook Page, Messenger bot, or other Meta platforms.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>2. Information We Collect</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography variant={"secondary"}>We may collect the following types of information:</Typography>
-                        <ol className="list-disc list-inside space-y-2">
-                            <li><strong>Personal Information:</strong> This includes information you provide directly, such as your name, email address, phone number, and any other personal details you share through Meta platforms.</li>
-                            <li><strong>Meta Platform Data:</strong> Data collected through Meta platforms, such as user interactions with our Messenger bot, Facebook Page, or other Meta services.</li>
-                            <li><strong>Usage Data:</strong> Information about your interactions with our app, including how you use our services and your preferences.</li>
-                            <li><strong>Device Information:</strong> Information about your device, including IP address, browser type, operating system, and other technical details.</li>
-                        </ol>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>3. How We Use Your Information</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography variant={"secondary"}>We use your information for the following purposes:</Typography>
-                        <ol className="list-disc list-inside">
-                            <li>To provide, operate, and maintain our services on Meta platforms.</li>
-                            <li>To improve, personalize, and expand our services.</li>
-                            <li>To process transactions and manage your orders.</li>
-                            <li>To communicate with you, including sending updates and promotional materials related to our services.</li>
-                            <li>To analyze user interactions and improve our app's performance and user experience.</li>
-                        </ol>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>4. How We Share Your Information</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography variant={"secondary"}>We do not sell or rent your personal information. We may share your information with:</Typography>
-                        <ol className="list-disc list-inside">
-                            <li><strong>Service Providers:</strong> Third-party vendors who assist us in providing and managing our services on Meta platforms.</li>
-                            <li><strong>Meta Platforms:</strong> Information may be shared with Meta for the purpose of operating and maintaining our integrations with their services.</li>
-                            <li><strong>Legal Requirements:</strong> When required by law or to protect our rights, property, or safety.</li>
-                            <li><strong>Business Transfers:</strong> In connection with a merger, acquisition, or sale of all or a portion of our assets.</li>
-                        </ol>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>5. Your Choices and Rights</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>You have the following rights regarding your personal information:</Typography>
-                        <ol className="list-disc list-inside">
-                            <li><strong>Access and Correction:</strong> You can request access to your personal information and request corrections if needed.</li>
-                            <li><strong>Opt-Out:</strong> You can opt-out of receiving promotional communications from us.</li>
-                            <li><strong>Data Deletion:</strong> You can request the deletion of your personal information by contacting us.</li>
-                        </ol>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>6. Security</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>We take reasonable measures to protect your personal information from unauthorized access, use, or disclosure. However, no security system is impenetrable, and we cannot guarantee the absolute security of your information.</Typography>
-                    </div>
-                </div>
-                <div>
-                    <Typography variant={"secondary"}>7. Third-Party Links</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Our app may contain links to third-party websites or services, including Meta services. We are not responsible for the privacy practices or content of these third parties.</Typography>
-                    </div>
-                </div>
-                <div>
-                    <Typography variant={"secondary"}>8. Meta Platform Compliance</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Our app complies with Meta’s Platform Policies and Data Use Policy. For more information on how Meta handles your data, please refer to their <a href="https://www.facebook.com/privacy/policy" className="text-blue-500 underline">Data Policy</a> and <a href="https://developers.facebook.com/policy/" className="text-blue-500 underline">Platform Policies</a>.</Typography>
-                    </div>
-                </div>
-                <div>
-                    <Typography variant={"secondary"}>9. Children’s Privacy</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Our services are not intended for children under the age of 13. We do not knowingly collect personal information from children under 13. If we become aware that we have collected such information, we will take steps to delete it.</Typography>
-                    </div>
-                </div>
-                <div>
-                    <Typography variant={"secondary"}>10. Changes to This Privacy Policy</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>We may update this Privacy Policy from time to time. Any changes will be posted on this page with an updated effective date. Your continued use of our services constitutes your acceptance of the revised Privacy Policy.</Typography>
-                    </div>
-                </div>
-                <div>
-                    <Typography variant={"secondary"}>11. Contact Us</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>If you have any questions or concerns about this Privacy Policy or our practices, please contact us at:</Typography>
-                        <div className='grid'>
-                            <Typography><strong>Megz Courses</strong></Typography>
-                            <Typography>Email: <a href={`mailto:${env.NEXT_PUBLIC_ZOHO_MAIL}`} className="text-info underline">{env.NEXT_PUBLIC_ZOHO_MAIL}</a></Typography>
-                        </div>
-                    </div>
-                </div>
+            <div className='w-full'>
+                <iframe
+                    className='border-none w-full h-[85vh]'
+                    allowFullScreen
+                    src="https://docs.google.com/document/d/1XKdMsc_CI9X9zhSAG-bgNUAYn0g4puq-Re9gE_SEg28/preview"
+                />
             </div>
         </LandingLayout>
     );
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -1,8 +1,4 @@
 import LandingLayout from '@/components/landingPageComponents/LandingLayout';
-import { PaperContainer } from '@/components/ui/PaperContainers';
-import { ConceptTitle, Typography } from '@/components/ui/Typoghraphy';
-import { env } from '@/env.mjs';
-import { format } from 'date-fns';
 import Head from 'next/head';
 
 const TermsAndConditionsPage = () => {
@@ -13,84 +9,12 @@ const TermsAndConditionsPage = () => {
                 <meta name="description" content="Terms and Conditions for Megz Learning" />
                 <meta name="robots" content="index, follow" />
             </Head>
-            <div className="p-12 space-y-8">
-                <ConceptTitle className="text-2xl font-semibold mb-4">Terms and Conditions</ConceptTitle>
-
-                <Typography><strong>Effective Date:</strong> {format(new Date("7/aug/2024"), "PPP")}</Typography>
-
-                <div>
-                    <Typography variant={"secondary"}>1. Introduction</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Welcome to Megz Learning! These Terms and Conditions outline the rules and regulations for the use of our learning management system. By accessing or using our platform, you agree to comply with these Terms and Conditions.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>2. User Accounts</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Users are required to create an account to access specific pages of the platform. There are several types of accounts: Admin, Student (automatically created upon signup), Teacher, and Agent (created by the Admin as needed). All users must provide accurate information during account creation.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>3. Content and Usage</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>All course content on the platform is owned by the site owner and may be created by the owner, Admin, or Agents. Content must be appropriate and, if necessary, include age limits in the course name. Sharing of content outside the platform is strictly prohibited.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>4. Payment and Refund Policy</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Courses on the platform are paid. Refunds are handled according to the system owner's refund policy, which can be provided upon request by contacting the owner.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>5. User Conduct</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>Users are expected to adhere to standard behavior, including respect for others and adherence to the platform's rules. Non-adherence to these Terms and Conditions may result in suspension of the user's account.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>6. Data Collection and Privacy</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>We collect data including name, email, phone number, and optionally, an image and address. We also collect data on courses purchased, session times, and grades. This data is used for analytics and personalized offers and is stored in our cloud database. We do not share this data with third parties.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>7. Intellectual Property</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>We do not hold any trademarks, copyrights, or patents related to our platform. Intellectual property rights for user-generated content are not managed by the platform.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>8. Changes to Terms and Conditions</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>The platform owner reserves the right to modify these Terms and Conditions as needed. Users will be notified of any changes via email.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>9. Governing Law</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>These Terms and Conditions are governed by the laws of Egypt.</Typography>
-                    </div>
-                </div>
-
-                <div>
-                    <Typography variant={"secondary"}>10. Contact Us</Typography>
-                    <div className="py-2 px-10 space-y-4">
-                        <Typography>If you have any questions or concerns about these Terms and Conditions, please contact us at:</Typography>
-                        <div className='grid'>
-                            <Typography><strong>Megz Learning</strong></Typography>
-                            <Typography>Email: <a href={`mailto:${env.NEXT_PUBLIC_ZOHO_MAIL}`} className="text-info underline">{env.NEXT_PUBLIC_ZOHO_MAIL}</a></Typography>
-                        </div>
-                    </div>
-                </div>
+            <div className='w-full'>
+                <iframe
+                    className='border-none w-full h-[85vh]'
+                    allowFullScreen
+                    src="https://docs.google.com/document/d/1pVbvyEnM3_GtZpqJUlL4Nq7PprPXbeZOWagh20odE3k/preview"
+                />
             </div>
         </LandingLayout>
     );
