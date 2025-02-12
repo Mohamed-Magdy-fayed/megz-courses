7c56fff9513c6d7cb0481fd8b1bbc74190b8fee3
Mohamed Magdy Fayed <mohamed.ahmed.sc2@gmail.com>
2025-02-13 01:07:57 +0200

    updated the zoom revoke logic for zoom verification

--- src/lib/zoomAccountsHelpers.ts
+++ src/lib/zoomAccountsHelpers.ts
@@ -1,29 +1,30 @@
 import { env } from "@/env.mjs";
 import axios from "axios";
+import QueryString from "qs";
 
 export async function revokeZoomAccount(token: string) {
     const clientId = env.NEXT_PUBLIC_ZOOM_CLIENT_ID
     const clientSecret = env.NEXT_PUBLIC_ZOOM_CLIENT_SECRET
 
-    const input = `${clientId}:${clientSecret}`;
-    const encoded = Buffer.from(input, 'utf-8').toString('base64');
-
-    const data = JSON.stringify({
-        token,
-    })
+    const data = QueryString.stringify({
+        'token': token,
+        'client_id': clientId,
+        'client_secret': clientSecret
+    });
 
     const config = {
         method: 'post',
         maxBodyLength: Infinity,
         url: 'https://zoom.us/oauth/revoke',
         headers: {
-            'Authorization': `Basic ${encoded}`,
             'Content-Type': 'application/x-www-form-urlencoded',
         },
         data: data
     };
 
-    const response = await axios.request(config);
-
-    return response.data
+    try {
+        await axios.request(config);
+    } catch (error: any) {
+        console.log(error.response.data);
+    }
 }
